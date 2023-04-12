1345587c48fb5b65470b627fb54aef32ae2ff78d
YassineROCdev <118259418+YassineROCdev@users.noreply.github.com>
2023-04-12 13:54:14 +0200

    bijna klaar 2

--- js/main.js
+++ js/main.js
@@ -2,5 +2,10 @@ const profielkenmerken = {
     EmailAdress: '302888209@student.rocmondriaan.nl',
     userName: 'YassineRocdev',
     birthDate: '22-12-2004',
-    profilePhoto:,
+    profilePhoto: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
 }
+
+
+console.log(
+    "klaar met opdracht 2"
+);
