7af918be27ab55a90e8e7b2d58d6db080dde3be1
YassineROCdev <118259418+YassineROCdev@users.noreply.github.com>
2023-04-13 10:22:03 +0200

    profiel pagina af

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
 const profielkenmerken = {
-    EmailAdress: '302888209@student.rocmondriaan.nl',
-    userName: 'YassineRocdev',
-    birthDate: '22-12-2004',
-    profilePhoto: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
+    EmailAdress: "302888209@student.rocmondriaan.nl",
+    userName: "YassineRocdev",
+    birthDate: "22-12-2004",
+    profilePhoto: "hier komt mijn profielfoto",
+    biografie: "i live in the netherlands and i am 18 years old",
+    projects: "hoger lager boter kaas en eiren",
+    opleidingen: "software developer",
+    vaardingheden: "goed in rekenen goed in engels basic coding skills",
+    bannerkleur: "blue",
+    socialmedias: "instagram and twitter"
+
+};
+
+for (key in profielkenmerken) {
+    console.log('----------');
+    console.log(key + ': ' + profielkenmerken[key]);
 }
 
-
-console.log(
-    "klaar met opdracht 2"
-);
+// console.log(
+//     "klaar met opdracht 2"
+// );
